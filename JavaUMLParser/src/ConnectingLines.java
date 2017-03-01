679647bb5e965e8ad8b5c722ce4d472df2156fbf
Abhishek <Abhishek@10.0.0.159>
2017-03-01 01:51:33 -0800

    First commit of these 2 files

---
+++ JavaUMLParser/src/ConnectingLines.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+
+/**
+ * @author Abhishek
+ *
+ */
+public class ConnectingLines {
+	
+	private static final String DEPENDENCY = "DEPENDENCY";
+	private static final String ASSOCIATION = "ASSOCIATION";
+	private static final String EXTENDS = "EXTENDS";
+	private static final String IMPLEMENTS = "IMPLEMENTS";
+	/**
+	 * @return the dependency
+	 */
+	public static String getDependency() {
+		return DEPENDENCY;
+	}
+	/**
+	 * @return the association
+	 */
+	public static String getAssociation() {
+		return ASSOCIATION;
+	}
+	/**
+	 * @return the extends
+	 */
+	public static String getExtends() {
+		return EXTENDS;
+	}
+	/**
+	 * @return the implements
+	 */
+	public static String getImplements() {
+		return IMPLEMENTS;
+	}
+
+}
---
+++ JavaUMLParser/src/NodesConnection.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+
+/**
+ * @author Abhishek
+ *
+ */
+public class NodesConnection {
+	
+	private ClassGeneration sourceNode;
+	private ClassGeneration destinationNode;
+	private String connectingLine;
+	private String multiplicityLevel;
+	/**
+	 * @return the sourceNode
+	 */
+	public ClassGeneration getSourceNode() {
+		return sourceNode;
+	}
+	/**
+	 * @param sourceNode the sourceNode to set
+	 */
+	public void setSourceNode(ClassGeneration sourceNode) {
+		this.sourceNode = sourceNode;
+	}
+	/**
+	 * @return the destinationNode
+	 */
+	public ClassGeneration getDestinationNode() {
+		return destinationNode;
+	}
+	/**
+	 * @param destinationNode the destinationNode to set
+	 */
+	public void setDestinationNode(ClassGeneration destinationNode) {
+		this.destinationNode = destinationNode;
+	}
+	/**
+	 * @return the connectingLine
+	 */
+	public String getConnectingLine() {
+		return connectingLine;
+	}
+	/**
+	 * @param connectingLine the connectingLine to set
+	 */
+	public void setConnectingLine(String connectingLine) {
+		this.connectingLine = connectingLine;
+	}
+	/**
+	 * @return the multiplicityLevel
+	 */
+	public String getMultiplicityLevel() {
+		return multiplicityLevel;
+	}
+	/**
+	 * @param multiplicityLevel the multiplicityLevel to set
+	 */
+	public void setMultiplicityLevel(String multiplicityLevel) {
+		this.multiplicityLevel = multiplicityLevel;
+	}
+
+}
