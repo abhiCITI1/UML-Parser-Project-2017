9483a2b6c1dca759293261e3aebc338cb56ca5e0
Abhishek <Abhishek@10.0.0.159>
2017-03-15 23:43:08 -0700

    Modularity and fixes for issues while executing test case 4

---
+++ JavaUMLParser/src/UMLClassGetterSetterBuilder.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import japa.parser.ast.body.FieldDeclaration;
+import japa.parser.ast.body.MethodDeclaration;
+
+/**
+ * 
+ */
+
+/**
+ * @author Abhishek
+ *
+ */
+public class UMLClassGetterSetterBuilder {
+	
+	private String umlVariables = "" ;
+	UMLClassAttributesBuilder umlClassAttributesBuilder = new UMLClassAttributesBuilder();
+	private List<MethodDeclaration> getterSetterMethodList =  new ArrayList<MethodDeclaration>();
+	UMLClassMethodsBuilder umlClassMethodsBuilder = new UMLClassMethodsBuilder();
+	
+	public String getSetterGetterBuilt(List<ClassGeneration> generatedClassList, boolean fieldAvailable)
+	{
+		
+		for(ClassGeneration generatedClass : generatedClassList)
+		{
+			if(!generatedClass.isInterfaceFlag())
+			{
+				umlVariables = umlVariables + "class "+ generatedClass.getClassName() + "{\n";
+			}
+			else
+			{
+				umlVariables = umlVariables + "interface "+ generatedClass.getClassName() + "<<interface>>" + "{\n";
+			}
+			if(generatedClass.getFieldNames() !=null)
+			{
+				fieldAvailable = true;
+				umlVariables = umlVariables + umlClassAttributesBuilder.getUMLClassAttributes(generatedClass.getFieldNames());
+				umlVariables = umlVariables+"\n";
+			}
+			if(generatedClass.getMethodNames() !=null)
+			{
+				/*
+				 * Below code block is to convert the private variable based
+				 * on its public setter and getter method
+				 * */
+				List<MethodDeclaration> methodList = generatedClass.getMethodNames();
+				if(fieldAvailable)
+				{
+					for(MethodDeclaration currentMethod : methodList)
+					{
+						for(FieldDeclaration eachField : generatedClass.getFieldNames())
+						{
+							String fieldSetterMethod = "set" + eachField.getVariables().get(0).toString();
+							String fieldGetterMethod = "get" + eachField.getVariables().get(0).toString();
+							
+							if(currentMethod.getName().equalsIgnoreCase(fieldGetterMethod) || currentMethod.getName().equalsIgnoreCase(fieldSetterMethod))
+							{
+								getterSetterMethodList.add(currentMethod);
+								//System.out.println(Modifier.toString(1));
+								eachField.setModifiers(1);
+							}
+							
+						}
+					}
+					for(MethodDeclaration getterSetterMethod : getterSetterMethodList)
+					{
+						methodList.remove(getterSetterMethod);
+					}
+				}
+				
+				
+				
+				umlVariables = umlVariables + umlClassMethodsBuilder.getUMLClassMethods(generatedClass.getMethodNames());
+			}
+			umlVariables += "}\n";
+		}
+		return umlVariables;
+	}
+
+}
--- JavaUMLParser/src/UMLStringOutput.java
+++ JavaUMLParser/src/UMLStringOutput.java
@@ -70,62 +70,14 @@ public class UMLStringOutput {
 		}
 		
 		
+		/** 
+		 * This method invocation is to build the parsed plantUML input based on the class type or interface type and 
+		 * to convert the private variable having public setter and getter methods into a public variable and removing those setter getter
+		 * methods from the method list 
+		 */
 		
+		umlVariables = new UMLClassGetterSetterBuilder().getSetterGetterBuilt(generatedClassList,fieldAvailable);
 		
-		
-		for(ClassGeneration generatedClass : generatedClassList)
-		{
-			if(!generatedClass.isInterfaceFlag())
-			{
-				umlVariables = umlVariables + "class "+ generatedClass.getClassName() + "{\n";
-			}
-			else
-			{
-				umlVariables = umlVariables + "interface "+ generatedClass.getClassName() + "<<interface>>" + "{\n";
-			}
-			if(generatedClass.getFieldNames() !=null)
-			{
-				fieldAvailable = true;
-				umlVariables = umlVariables + umlClassAttributesBuilder.getUMLClassAttributes(generatedClass.getFieldNames());
-				umlVariables = umlVariables+"\n";
-			}
-			if(generatedClass.getMethodNames() !=null)
-			{
-				/*
-				 * Below code block is to convert the private variable based
-				 * on its public setter and getter method
-				 * */
-				List<MethodDeclaration> methodList = generatedClass.getMethodNames();
-				if(fieldAvailable)
-				{
-					for(MethodDeclaration currentMethod : methodList)
-					{
-						for(FieldDeclaration eachField : generatedClass.getFieldNames())
-						{
-							String fieldSetterMethod = "set" + eachField.getVariables().get(0).toString();
-							String fieldGetterMethod = "get" + eachField.getVariables().get(0).toString();
-							
-							if(currentMethod.getName().equalsIgnoreCase(fieldGetterMethod) || currentMethod.getName().equalsIgnoreCase(fieldSetterMethod))
-							{
-								getterSetterMethodList.add(currentMethod);
-								//System.out.println(Modifier.toString(1));
-								eachField.setModifiers(1);
-							}
-							
-						}
-					}
-					for(MethodDeclaration getterSetterMethod : getterSetterMethodList)
-					{
-						methodList.remove(getterSetterMethod);
-					}
-				}
-				
-				
-				
-				umlVariables = umlVariables + umlClassMethodsBuilder.getUMLClassMethods(generatedClass.getMethodNames());
-			}
-			umlVariables += "}\n";
-		}
 			
 		List<NodesConnection> connectedNodesDesc = plantUMLFigure.getConnectedLineswithNodes();
 		
