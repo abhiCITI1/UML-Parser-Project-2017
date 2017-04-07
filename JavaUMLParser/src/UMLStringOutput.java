11ab34d3d57410a8bdfcb89129e56512538995ef
Abhishek <Abhishek@10.250.151.249>
2017-04-07 00:21:04 -0700

    Refactoring

--- JavaUMLParser/src/UMLStringOutput.java
+++ JavaUMLParser/src/UMLStringOutput.java
@@ -17,14 +17,14 @@ import net.sourceforge.plantuml.SourceStringReader;
  * @author Abhishek
  */
 public class UMLStringOutput {
-	
-	
+
+
 	UMLClassAttributesBuilder umlClassAttributesBuilder = new UMLClassAttributesBuilder();
 	UMLClassMethodsBuilder umlClassMethodsBuilder = new UMLClassMethodsBuilder();
 	UMLClassConnectionLineBuilder umlClassConnectionLineBuilder = new UMLClassConnectionLineBuilder();
-	
+
 	private String  finalPlantUMLTemplate = "";
-	
+
 	/**
 	 * @param plantUMLFigure
 	 * @param sourceFolderName
@@ -32,50 +32,44 @@ public class UMLStringOutput {
 	 * @throws IOException 
 	 * @throws ClassNotFoundException 
 	 */
-	@SuppressWarnings("resource")
 	public void generatePlantUMLTemplateFile(PlantUMLFigure plantUMLFigure, String sourceFolderName, String outputImageFileName ) throws IOException, ClassNotFoundException
 	{
 		String umlVariables = "";
-		//Fetching the generated class list from the Plant UML Figure
 		List<ClassGeneration> generatedClassList =  plantUMLFigure.getGeneratedClass();
 		boolean fieldAvailable = false;
-		//List<MethodDeclaration> getterSetterMethodList =  new ArrayList<MethodDeclaration>();
-		
-		
+
+
 		/** 
 		 * This method invocation is to build the parsed plantUML input based on the class type or interface type and 
 		 * to convert the private variable having public setter and getter methods into a public variable and removing those setter getter
-		 * methods from the method list, creating string input based on parsed fieldVariables, methods, constructors for planUML 
+		 * methods from the method list, creating string input based on parsed fieldVariables, methods, constructors for plantUML 
 		 */
-		
+
 		umlVariables = new PlantUMLClassFinalTemplateBuilder().buildPlantUMLClassFinalTemplate(generatedClassList,fieldAvailable);
-		
-			
+
+
 		List<NodesConnection> connectedNodesDesc = plantUMLFigure.getConnectedLineswithNodes();
-		
+
 		if(connectedNodesDesc !=null && connectedNodesDesc.size()!=0)
 		{
 			umlVariables = umlVariables + umlClassConnectionLineBuilder.getConnectedLinesDesc(connectedNodesDesc);
 		}
-		
-		
+
+
 		finalPlantUMLTemplate = "@startuml\nskinparam classAttributeIconSize 0\n" + umlVariables + "\n@enduml";
-		
+
 		SourceStringReader sourceReader = new SourceStringReader(finalPlantUMLTemplate);
-		
+
 		FileOutputStream fos = null;
 		try {
 			fos = new FileOutputStream(outputImageFileName+".png");
 		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		try 
-		
 		{
-		sourceReader.generateImage(fos, new FileFormatOption(FileFormat.PNG , false));
+			sourceReader.generateImage(fos, new FileFormatOption(FileFormat.PNG , false));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
