f99260c96adb7e5fd82e30aeec13dc559c8d44ed
Abhishek <Abhishek@10.0.0.159>
2017-04-14 01:29:29 -0700

    variability handling for class generation and comparision

--- JavaUMLParser/src/PlantUMLFigureTemplate.java
+++ JavaUMLParser/src/PlantUMLFigureTemplate.java
@@ -8,7 +8,7 @@ import java.util.List;
  *This is the class which has the complete class or interface figures
  *with respective class names and the association/dependency between them
  */
-public class PlantUMLFigure {
+public class PlantUMLFigureTemplate {
 	
 	private List<ClassGeneration> generatedClass;
 	private List<NodesConnection> connectedLineswithNodes;
@@ -16,7 +16,7 @@ public class PlantUMLFigure {
 	 * @param generatedClass
 	 * @param connectedLineswithNodes
 	 */
-	public PlantUMLFigure() {
+	public PlantUMLFigureTemplate() {
 		super();
 		this.generatedClass = new ArrayList<ClassGeneration>(0);
 		this.connectedLineswithNodes = new ArrayList<NodesConnection>(0);
@@ -31,12 +31,25 @@ public class PlantUMLFigure {
 	 * @param generatedClass the generatedClass to set
 	 */
 	public void setGeneratedClass(List<ClassGeneration> generatedClass) {
+//		StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+//		int stackTop = stackTraceElements.length;
+//		String sourceParticipant = stackTraceElements[stackTop-1].getClassName();
+//		String destinationParticipant = PlantUMLFigureTemplate.class.getName();
+//		String message = "setGeneratedClass(" + generatedClass + ")";
+//		Main.sdg.generateSequenceDiagram(sourceParticipant, destinationParticipant,message);
 		this.generatedClass = generatedClass;
 	}
 	/**
 	 * @return the connectedLineswithNodes
 	 */
 	public List<NodesConnection> getConnectedLineswithNodes() {
+//		StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+//		int stackTop = stackTraceElements.length;
+//		String sourceParticipant = stackTraceElements[2].getClassName();
+//		String destinationParticipant = PlantUMLFigureTemplate.class.getName();
+//		String message = "getConnectedLineswithNodes()";
+//		Main.sdg.generateSequenceDiagram(sourceParticipant, destinationParticipant,message);
+		
 		return connectedLineswithNodes;
 	}
 	/**
@@ -47,5 +60,21 @@ public class PlantUMLFigure {
 	}
 	
 	
-	
+	public ClassGeneration getClassGeneration(String s)
+	{
+//		StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+//		int stackTop = stackTraceElements.length;
+//		String sourceParticipant = stackTraceElements[2].getClassName();
+//		String destinationParticipant = PlantUMLFigureTemplate.class.getName();
+//		String message = "getClassGeneration("+ s + ")";
+//		Main.sdg.generateSequenceDiagram(sourceParticipant, destinationParticipant,message);
+		for (ClassGeneration currentclass : generatedClass) 
+		{
+			if(currentclass.getClassName().equals(s))
+			{
+				return currentclass;
+			}
+		}
+		return null;
+	}
 }
